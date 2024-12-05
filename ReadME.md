f6f081c127d40c27ef8ce383774df8043db8af3d
ramesh6435 <ckramesh0006@gmail.com>
2024-12-05 07:55:00 +0530

    change to class component

--- ReadME.md
+++ ReadME.md
@@ -1,54 +1,106 @@
+
+# Web3 Simplifier
+
+`web3-simplifier` is a simple utility package that simplifies interactions with the Ethereum blockchain using the popular `web3.js` library. It provides a set of easy-to-use methods for common blockchain tasks such as querying balances, sending transactions, interacting with smart contracts, and more.
+
+## Installation
+
+You can install the `web3-simplifier` package via `npm`:
+
+```bash
+npm install web3-simplifier
+```
+
 ```js
-const {getBalance, getChainId} = require('web3-simplifier')
+const Web3Utils = require('web3-simplifier');
 
-var balance = await getBalance("rpcurl", "address")
+const web3Utils = new Web3Utils('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
 
-var chainId = await getChainId("rpcurl")
+const balance = await web3Utils.getBalance('0x1234...abcd');
+console.log(balance); // e.g., "1.0"
 
-var currentBlock = await getCurrentBlock("rpcurl")
 
-var gasPrice = await getGasPrice("rpcurl")
+const chainId = await web3Utils.getChainId();
+console.log(chainId); // e.g., 1
 
-var blockTransactionCount = await getBlockTransactionCount("rpcurl","blockNumber")
 
-var blockDetails = await getBlock("rpcurl", "blockNumber")
+const currentBlock = await web3Utils.getCurrentBlock();
+console.log(currentBlock); // e.g., 1234567
 
-var transactionDetails = await getTransaction("rpcurl", "hash")
 
-var pendingTransactionDetails = await getPendingTransactions("rpcurl")
+const gasPrice = await web3Utils.getGasPrice();
+console.log(gasPrice); // e.g., "20000000000" (20 Gwei)
 
-var transactionsFromBlock = await getTransactionFromBlock("rpcurl","blockNumber","index")
+
+const txCount = await web3Utils.getBlockTransactionCount(12345); // 12345 is block number
+console.log(txCount); // e.g., 10
+
+
+const blockDetails = await web3Utils.getBlock(12345);  // 12345 is block number
+console.log(blockDetails);
+
+
+const txDetails = await web3Utils.getTransaction('0x1234...abcd'); // transaction hash
+console.log(txDetails);
+
+
+const pendingTxs = await web3Utils.getPendingTransactions();
+console.log(pendingTxs);
+
+
+const txFromBlock = await web3Utils.getTransactionFromBlock(12345, 0); // 12345 is block number , 0 is index
+console.log(txFromBlock);
+
 // index means block transactions list index number if block contains 100 transaction index is 5 then return  5th transaction details 
 
-var transactionReceipt = await getTransactionReceipt("rpcurl","hash")
+const receipt = await web3Utils.getTransactionReceipt('0x1234...abcd');  // transaction hash
+console.log(receipt);
+
 // this return the logs and other details of transaction
 
-var transactionCount = await getTransactionCount("rpcurl", "address")
+const txCount = await web3Utils.getTransactionCount('0x1234...abcd'); // user address
+console.log(txCount); // e.g., 5
+
 // this return the nounce of the address that means addres transaction count 
 
-var createAddress = await createAddress("rpcurl")
+const newAddress = await web3Utils.createAddress();
+console.log(newAddress);
 
-
-var payload = {
-            rpcurl: "rpcurl",
+var transferData = {
             from_address: "From Address",
             to_address: "To Address",
             privatekey: "From Address Private Key",
             amount: "Transfer Amount in String Format like 0.01 or 1",
             gas: "gas Amount to Spend like 21000 for basic Transaction need"
         }
-var transferAmount = await transferAmount(payload)
 
-var decodeLogsParameter = await decodeLogsParameter("rpcurl", "type", "topic")
+const transferData = {
+    from_address: '0x1234...abcd',
+    to_address: '0x5678...efgh',
+    privatekey: '0xabcdef...',
+    amount: '1',
+    gas: '21000'
+};
+
+const transferReceipt = await web3Utils.transferAmount(transferData);
+console.log(transferReceipt);
+
+const decodedLog = await web3Utils.decodeLogsParameter('uint256', '0x1234...abcd'); //uint256 is type, topic
+console.log(decodedLog);
 // type must be uint256 or string or address 
 
-var contractCallMethods = await contractCall("rpcurl", "ABI", "ContractAddress", "function Name", ["parameter", "parameter"])
+const result = await web3Utils.contractCall(Abi, '0xContractAddress', 'balanceOf', ['0x1234...abcd']); // abi, contract address, function name, params
+console.log(result);
 // parameters in Array format what are the inputs are given. if no parameters are given then give the empty array.
 
 
-var getContractBalance = await getContractBalance("rpcurl", "ABI", "ContractAddress", "userAddress")
+const tokenBalance = await web3Utils.getContractBalance(Abi, '0xTokenContractAddress', '0x1234...abcd'); // abi, contract address, user address
+console.log(tokenBalance);
 
-var contractSendMethods = await contractSend("rpcurl", "ABI", "ContractAddress", "function Name","from address", "privatekey", ["parameter", "parameter"])
+const txReceipt = await web3Utils.contractSend(Abi, '0xContractAddress', 'approve', '0x1234...abcd', '0xPrivateKey', ['0x5678...efgh', '1000000000000000000']);
+                                                // abi, contract address, function name, from address, privatekey, params
+console.log(txReceipt);
+
 
 
 ```
--- src/index.js
+++ src/index.js
@@ -1,257 +1,201 @@
 const web3 = require("web3");
-const Abi = require('./erc20.json')
+const Abi = require('./erc20.json');
 
-const getBalance = async (rpcurl, address) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const bal = await web3Instance.eth.getBalance(address);
-        const balance = web3Instance.utils.fromWei(bal, "ether");
-        return balance
-    } catch (error) {
-        throw error.message
+class Web3Utils {
+    constructor(rpcurl) {
+        this.web3Instance = new web3(rpcurl);
     }
-}
 
-const getChainId = async (rpcurl) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const chainId = await web3Instance.eth.getChainId();
-        return chainId
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getCurrentBlock = async (rpcurl) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const block = await web3Instance.eth.getBlockNumber();
-        return block
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getGasPrice = async (rpcurl) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const gasPrice = await web3Instance.eth.getGasPrice();
-        return gasPrice
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getBlockTransactionCount = async (rpcurl, blockNumber) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const count = await web3Instance.eth.getBlockTransactionCount(blockNumber);
-        return count
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getBlock = async (rpcurl, blockNumber) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const block = await web3Instance.eth.getBlock(blockNumber);
-        return block
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getTransaction = async (rpcurl, hash) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const transaction = await web3Instance.eth.getTransaction(hash);
-        return transaction
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getPendingTransactions = async (rpcurl) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const transactions = await web3Instance.eth.getPendingTransactions();
-        return transactions
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getTransactionFromBlock = async (rpcurl, blockNumber, index) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const transactions = await web3Instance.eth.getTransactionFromBlock(blockNumber, index);
-        return transactions
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getTransactionReceipt = async (rpcurl, hash) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const receipt = await web3Instance.eth.getTransactionReceipt(hash);
-        return receipt
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getTransactionCount = async (rpcurl, address) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const count = await web3Instance.eth.getTransactionCount(address);
-        return count
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const createAddress = async (rpcurl) => {
-    try {
-        const web3Instance = new web3(rpcurl);
-        const address = await web3Instance.eth.accounts.create();
-        return address
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const transferAmount = async (data) => {
-    try {
-        if (!data.rpcurl || !data.from_address || !data.to_address || !data.privatekey || !data.amount || !data.gas) {
-            return "All fields are required"
-        }
-        const web3Instance = new web3(data.rpcurl)
-        const datas = {}
-        const value = await web3Instance.utils.toWei(data.amount, 'ether')
-        await web3Instance.eth.accounts.signTransaction(
-            {
-                from: data.from_address,
-                to: data.to_address,
-                value,
-                gas: data?.gas
-            },
-            data.privatekey,
-            (error, receipt) => {
-                if (error) {
-                    return error.message
-                } else {
-                    datas.signedtransaction = receipt
-                }
-            }
-        )
-        if (datas.signedtransaction !== undefined) {
-            await web3Instance.eth.sendSignedTransaction(
-                datas.signedtransaction.rawTransaction,
-                (error, receipt) => {
-                    if (error) {
-                        return error.message
-                    } else {
-                        datas.transaction_receipt = receipt
-                    }
-                }
-            )
-        }
-        if (datas.signedtransaction && datas.transaction_receipt) {
-            return datas
-        }
-    } catch (error) {
-        return error.message
-    }
-}
-
-const decodeLogsParameter = async (rpcurl, type, topic) => {
-    try {
-        const web3Instance = new web3(rpcurl)
-        const logs = await web3Instance.eth.abi.decodeParameter(type, topic)
-        return logs
-    } catch (error) {
-        return error.message
-    }
-}
-
-const contractCall = async (rpcurl, Abi, address, functionName, params) => {
-    try {
-        if (!rpcurl || !Abi || !address || !functionName) {
-            return "All fields are required"
-        }
-        const web3Instance = new web3(rpcurl)
-        const contractInstance = new web3Instance.eth.Contract(Abi, address)
-        const result = await contractInstance.methods[functionName](...params).call()
-        return result
-    } catch (error) {
-        return error.message
-    }
-}
-
-const getContractBalance = async (rpcurl, Abi, ContractAddress, address) => {
-    try {
-        const web3Instance = new web3(rpcurl)
-        const contractInstance = new web3Instance.eth.Contract(Abi, ContractAddress)
-        const result = await contractInstance.methods.balanceOf(address).call()
-        const decimal = await contractInstance.methods.decimals().call()
-        const balance = result / (10 ** decimal)
-        return balance
-    } catch (error) {
-        return error.message
-    }
-}
-
-const contractSend = async (rpcurl, Abi, address, functionName, fromAddress, privatekey, params) => {
-    if (!rpcurl || !Abi || !address || !functionName || !fromAddress || !privatekey) {
-        return "All fields are required"
-    }
-    return new Promise(async (resolve, reject) => {
+    async getBalance(address) {
         try {
-            const web3Instance = new web3(rpcurl)
-            var datas = {}
-            const contract = new web3Instance.eth.Contract(Abi, address);
-            var gasAmount = await contract.methods[functionName](...params).estimateGas({ from: fromAddress });
+            const bal = await this.web3Instance.eth.getBalance(address);
+            const balance = this.web3Instance.utils.fromWei(bal, "ether");
+            return balance;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getChainId() {
+        try {
+            const chainId = await this.web3Instance.eth.getChainId();
+            return chainId;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getCurrentBlock() {
+        try {
+            const block = await this.web3Instance.eth.getBlockNumber();
+            return block;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getGasPrice() {
+        try {
+            const gasPrice = await this.web3Instance.eth.getGasPrice();
+            return gasPrice;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getBlockTransactionCount(blockNumber) {
+        try {
+            const count = await this.web3Instance.eth.getBlockTransactionCount(blockNumber);
+            return count;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getBlock(blockNumber) {
+        try {
+            const block = await this.web3Instance.eth.getBlock(blockNumber);
+            return block;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getTransaction(hash) {
+        try {
+            const transaction = await this.web3Instance.eth.getTransaction(hash);
+            return transaction;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getPendingTransactions() {
+        try {
+            const transactions = await this.web3Instance.eth.getPendingTransactions();
+            return transactions;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getTransactionFromBlock(blockNumber, index) {
+        try {
+            const transaction = await this.web3Instance.eth.getTransactionFromBlock(blockNumber, index);
+            return transaction;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getTransactionReceipt(hash) {
+        try {
+            const receipt = await this.web3Instance.eth.getTransactionReceipt(hash);
+            return receipt;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async getTransactionCount(address) {
+        try {
+            const count = await this.web3Instance.eth.getTransactionCount(address);
+            return count;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async createAddress() {
+        try {
+            const address = await this.web3Instance.eth.accounts.create();
+            return address;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    async transferAmount(data) {
+        try {
+            if (!data.from_address || !data.to_address || !data.privatekey || !data.amount || !data.gas) {
+                return "All fields are required";
+            }
+            const value = await this.web3Instance.utils.toWei(data.amount, 'ether');
+            const signedTransaction = await this.web3Instance.eth.accounts.signTransaction(
+                {
+                    from: data.from_address,
+                    to: data.to_address,
+                    value,
+                    gas: data?.gas
+                },
+                data.privatekey
+            );
+            const receipt = await this.web3Instance.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+            return { signedTransaction, transaction_receipt: receipt };
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    async decodeLogsParameter(type, topic) {
+        try {
+            const logs = await this.web3Instance.eth.abi.decodeParameter(type, topic);
+            return logs;
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    async contractCall(Abi, address, functionName, params) {
+        try {
+            if (!Abi || !address || !functionName) {
+                return "All fields are required";
+            }
+            const contractInstance = new this.web3Instance.eth.Contract(Abi, address);
+            const result = await contractInstance.methods[functionName](...params).call();
+            return result;
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    async getContractBalance(Abi, ContractAddress, address) {
+        try {
+            const contractInstance = new this.web3Instance.eth.Contract(Abi, ContractAddress);
+            const result = await contractInstance.methods.balanceOf(address).call();
+            const decimal = await contractInstance.methods.decimals().call();
+            const balance = result / (10 ** decimal);
+            return balance;
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    async contractSend(Abi, address, functionName, fromAddress, privatekey, params) {
+        if (!Abi || !address || !functionName || !fromAddress || !privatekey) {
+            return "All fields are required";
+        }
+        try {
+            const contract = new this.web3Instance.eth.Contract(Abi, address);
+            const gasAmount = await contract.methods[functionName](...params).estimateGas({ from: fromAddress });
             const txObject = {
                 from: fromAddress,
-                nonce: await web3Instance.eth.getTransactionCount(fromAddress),
+                nonce: await this.web3Instance.eth.getTransactionCount(fromAddress),
                 to: address,
                 gas: gasAmount,
                 value: "0x0",
                 data: contract.methods[functionName](...params).encodeABI(),
-            }
-
-            await web3Instance.eth.accounts.signTransaction(txObject, privatekey, async (err, res1) => {
-                if (err) {
-                    console.log('err', err)
-                    reject(err?.message)
-                }
-                else {
-                }
-                const raw = res1.rawTransaction
-                await web3Instance.eth.sendSignedTransaction(raw, async (err, txHash) => {
-                    if (err) {
-                        reject(err?.message)
-                    }
-                    else {
-                        resolve({
-                            signTransaction: res1,
-                            transactionHash: txHash
-                        })
-
-                    }
-                })
-            })
+            };
+            const signedTransaction = await this.web3Instance.eth.accounts.signTransaction(txObject, privatekey);
+            const txHash = await this.web3Instance.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+            return {
+                signTransaction: signedTransaction,
+                transactionHash: txHash
+            };
         } catch (error) {
-            reject(error.message)
+            throw new Error(error.message);
         }
-    })
-
+    }
 }
 
-
-module.exports = { getBalance, getChainId, getCurrentBlock, getGasPrice, getBlockTransactionCount, getBlock, getTransaction, getPendingTransactions, getTransactionFromBlock, getTransactionReceipt, getTransactionCount, createAddress, transferAmount, decodeLogsParameter, contractCall, getContractBalance, contractSend }
-
-
-
+module.exports = Web3Utils;
